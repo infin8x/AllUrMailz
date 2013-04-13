4283f78f1860fc2b04374437c522c136412e392f
Alex Mullans <alex@alexmullans.com>
2013-04-12 23:31:44 -0400

    New routes.

--- allurmailz.rb
+++ allurmailz.rb
@@ -3,11 +3,13 @@ require 'sinatra'
 require 'thin'
 require 'haml'
 require 'net/https'
+require 'json'
 
 class AllUrMailz < Sinatra::Base
 
     before do
         session[:oauth] ||= {}
+        session[:userinfo] ||= {}
         @consumer_key = 'V58Lo7BySs01zaXBOr1qhZHMMQIXSL'
         @consumer_secret = 'SMb4Z96sC1U1Rbk8To4WKaAyGqMw8L'
     end
@@ -24,7 +26,7 @@ class AllUrMailz < Sinatra::Base
 
         session[:oauth][:access_token] = split[3]
         session[:oauth][:access_token_secret] = split[1]
-        redirect '/'
+        redirect '/whoami'
     end
 
     def nonce
@@ -47,6 +49,11 @@ class AllUrMailz < Sinatra::Base
         redirect 'https://app.smartfile.com/oauth/authorize?oauth_token=' + CGI::escape(split[3])
     end
 
+    get '/signout' do 
+        session[:oauth] = {}  
+        session[:userinfo] = {}
+        redirect '/'
+    end
 
     get '/' do
         haml :index
@@ -56,8 +63,12 @@ class AllUrMailz < Sinatra::Base
         haml :item
     end
 
+    get '/account' do
+        haml :account
+    end
+
     get '/whoami' do
-        uri = URI.parse(URI.encode('https://app.smartfile.com/api/2/whoami?format=xml'))
+        uri = URI.parse(URI.encode('https://app.smartfile.com/api/2/whoami/?format=json&oauth_consumer_key=' + @consumer_key + '&oauth_token=' + session[:oauth][:access_token] + '&oauth_signature_method=PLAINTEXT&oauth_signature=' + @consumer_secret + '%26' + session[:oauth][:access_token_secret] + '&oauth_timestamp=' + Time.now.to_i.to_s + '&oauth_nonce=' + nonce.to_s + '&oauth_version=1.0'))
         puts uri.host
         puts uri.port
         puts uri.request_uri
@@ -65,11 +76,10 @@ class AllUrMailz < Sinatra::Base
         req = Net::HTTP::Get.new(uri.request_uri)
         http.use_ssl = true
 
-        req['Authorization'] = 'OAuth realm="https://app.smartfile.com/",oauth_consumer_key="' + @consumer_key + '",oauth_token="' + session[:oauth][:access_token] + '",oauth_signature_method="PLAINTEXT",oauth_signature="' + @consumer_secret + '%26' + session[:oauth][:access_token_secret] + '",oauth_timestamp="' + Time.now.to_i.to_s + '",oauth_nonce="' + nonce.to_s + '",oauth_version="1.0"'
-
         res = http.request(req)
-        puts res
-        puts res.body
+        response = JSON.parse(res.body)
+        session[:userinfo][:username] = response['user']['name']
+        session[:userinfo][:signedin] = true
+        redirect '/'
     end
-
 end
